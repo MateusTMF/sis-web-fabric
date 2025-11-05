a3f1d014e754596a8863d674db775884c58c2c79
MateusTMF <matefe1200@gmail.com>
2025-11-05 12:30:18 -0300

    Atualização

--- app/relatorios/producao/page.tsx
+++ app/relatorios/producao/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Sidebar from "@/components/sidebar"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
